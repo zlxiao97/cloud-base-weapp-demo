71bd8e08229218185092b0df06aa9cac10cb5820
肖子立 <xiaozlb@digitalchina.com>
2021-05-27 23:50:51 +0800

    pdflib demo

---
+++ cloudfunctions/pdfLib/index.js
@@ -0,0 +1,38 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+const pdfLib = require('pdf-lib')
+
+const { PDFDocument, StandardFonts, rgb } = pdfLib
+cloud.init()
+
+
+async function createPdf() {
+  const pdfDoc = await PDFDocument.create()
+  const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+
+  const page = pdfDoc.addPage()
+  const { width, height } = page.getSize()
+  const fontSize = 30
+  page.drawText('Creating PDFs in JavaScript is awesome!', {
+    x: 50,
+    y: height - 4 * fontSize,
+    size: fontSize,
+    font: timesRomanFont,
+    color: rgb(0, 0.53, 0.71),
+  })
+
+  return await pdfDoc.save()
+}
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const pdfBytes = await createPdf()
+  const buffer = Buffer.from(pdfBytes)
+  const {fileID}  = await cloud.uploadFile({
+    cloudPath: 'demo.pdf',
+    fileContent: buffer,
+  })
+  return {
+    fileID
+  }
+}
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,6 +3,7 @@ const app = getApp()
 
 Page({
   data: {
+    fileID: ''
   },
 
   onLoad: function () {
@@ -10,4 +11,38 @@ Page({
       return
     }
   },
+
+  handleTest: function () {
+    const that = this
+    wx.cloud.callFunction({
+      name: 'pdfLib',
+      data: {},
+      success(res) {
+        that.setData({
+          fileID: res.result.fileID
+        })
+      }
+    })
+  },
+  handlePreview: function () {
+    if (this.data.fileID) {
+      wx.cloud.downloadFile({
+        fileID: this.data.fileID,
+      }).then(res => {
+        const filePath = res.tempFilePath
+        console.log(filePath)
+        wx.openDocument({
+          filePath,
+          fileType: 'pdf'
+        })
+      }).catch(error => {
+        console.error(error)
+      })
+    } else {
+      wx.showToast({
+        title: '请先上传PDF',
+        icon: 'none'
+      })
+    }
+  }
 })
